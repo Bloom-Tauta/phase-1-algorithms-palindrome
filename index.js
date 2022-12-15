ee97fc0d34742535ef2c3895bcdcea3a0a4f22bc
Bloom <bloomtauta@gmail.com>
2022-12-15 11:29:10 +0300

    COmplerted assignment

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+function reverse(word) {
+  const wordArray = word.split("");
+  const reversedWordArray = wordArray.reverse();
+  const reversedWord = reversedWordArray.join("");
+  return reversedWord;
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+ 
+ const reversedWord = reverse(word);
+  if(word === reversedWord){
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 /* 
@@ -13,13 +27,32 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"))
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("ab"));
 }
 
 module.exports = isPalindrome;
+ // Write your algorithm here
+  // const length = word.length;
+  // for(let i = 0; i<length/2; i++) {
+  //   if (word[i] === word[length-1-i]){
+  //     return true;
+  //   }
+  //   else {
+  //     return false;
+  //   }
+  // }
